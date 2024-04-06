467c4773642107bf9d79f908b3d2a52522e1d981
Chethiya Hitihamu <154321309+ChethiyaCode@users.noreply.github.com>
2024-04-06 23:46:12 +0530

    Add SupplierDrawerForm component to SupplierEdit page

--- src/pages/suppliers/edit.tsx
+++ src/pages/suppliers/edit.tsx
@@ -1,5 +1,6 @@
 import { ProductDrawerForm } from "../../components/product/drawer-form";
+import { SupplierDrawerForm } from "../../components/supplier/drawer-form";
 
 export const SupplierEdit = () => {
-  return <ProductDrawerForm action="edit" />;
+  return <SupplierDrawerForm action="edit" />;
 };
