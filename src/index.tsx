37746064d3ada11b070abfc083ee3f5c78679e15
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2025-04-09 01:50:03 +0530

    perf: lazy load App component and add loading fallback
    Lazy load the App component to improve initial load performance by splitting the bundle. Added a LoadingPage component as a fallback for the Suspense boundary to enhance user experience during loading.

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { lazy } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App";
-
 import dayjs from "dayjs";
 import "./i18n";
+import { LoadingPage } from "./components/loading";
 
 import WeekDay from "dayjs/plugin/weekday";
 import LocaleData from "dayjs/plugin/localeData";
 import LocalizedFormat from "dayjs/plugin/localizedFormat";
+
+const App = lazy(() => import("./App"));
 
 dayjs.extend(WeekDay);
 dayjs.extend(LocaleData);
 dayjs.extend(LocalizedFormat);
 
 const container = document.getElementById("root");
-// eslint-disable-next-line
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <React.Suspense fallback={null}>
+    <React.Suspense fallback={<LoadingPage/>}>
       <App />
     </React.Suspense>
   </React.StrictMode>,
